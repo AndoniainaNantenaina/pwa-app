1c0d827c4871cf231b4ed3e1831115b7dc445b72
AndoniainaNantenaina <ando.andoniaina.nantenaina@esti.mg>
2021-10-14 13:28:41 +0200

    Adding some files

---
+++ etudiants.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Etudiants</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+        <nav>
+            <a href='index.html' class='selected'>Home</a>
+            <a href='etudiants.php' class='selected'>Etudiants</a>
+            <a href='apropos.php' class='selected'>A propos</a>
+        </nav>
+    
+</body>
+</html>
