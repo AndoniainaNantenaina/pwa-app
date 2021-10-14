31b2442db7f4b368cd374a665667222fc8fed73e
AndoniainaNantenaina <ando.andoniaina.nantenaina@esti.mg>
2021-10-14 08:36:11 +0200

    Correct vue version

--- js/app.js
+++ js/app.js
@@ -1,4 +1,9 @@
-const app = Vue.createApp({
-    data() {},
-    methods : {}
+const pwa = Vue.createApp({
+    data() {
+        return {
+            test : "Our PWA-APP"
+        }
+    }
 })
+
+pwa.mount('#pwa')
