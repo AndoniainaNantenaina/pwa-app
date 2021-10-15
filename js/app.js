754dfefc6f21e7a949aed35f47c9bd8e79ae9a01
tinahy10 <48431328+tinahy10@users.noreply.github.com>
2021-10-15 05:18:46 -0700

    add showEtudiant

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
-var pwa = new Vue({
+let pwa = new Vue({
   el: '#pwa',
   data: {
-    test: 'Our PWA-APP'
+    test: 'Our PWA-APP',
+    students: []
   },
   methods: {
     async addStudent() {
@@ -24,16 +25,43 @@ var pwa = new Vue({
     async showAllStudent() {
       const requestOptions = {
         method: "GET",
-        headers: { "Content-Type": "application/json" },
-        
+        headers: { "Content-Type": "application/json" }
       }
+
       const response = await fetch("http://127.0.0.1:5984/pwa-etudiant/_all_docs?include_docs=true", requestOptions);
       const data = await response.json();
       const rows = data.rows;
 
       rows.forEach(row => {
-        console.log(row["value"]);
+        this.showStudent(row["id"])
       });
+    },
+
+    async showStudent(id) {
+      const requestOptions = {
+        method: "GET",
+        headers: { "Content-Type": "application/json" }
+      }
+      
+      const response = await fetch(`http://127.0.0.1:5984/pwa-etudiant/${id}`, requestOptions);
+      const data = await response.json();
+
+      const student = {
+        "nom": data.nom,
+        "prenoms": data.prenoms,
+        "date_de_naissance": data.date_de_naissance,
+        "lieu_de_naissance": data.lieu_de_naissance,
+        "adresse": data.adresse,
+        "contact": data.contact,
+        "couriel": data.couriel
+      }
+
+      this.students.push(student);
+
+      console.log(data.nom);
     }
+  },
+  created() {
+    this.showAllStudent();
   }
-})
+});
